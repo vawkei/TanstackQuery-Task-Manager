8de15298f1584a5d212bf50c569dfa06b9628001
voke <vokes9810@gmail.com>
2025-11-24 05:49:58 +0100

    added Dockerfiles to backend,revers-proxy. removed cluster.ts and added docker-compose. All containers are running. yet to add frontend service

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -12,28 +12,11 @@ import session from "express-session";
 import connectRedis from "connect-redis";
 import { redisClient } from "./redis/redisClient";
 
-
-
 const authRoute = authRouter;
 const taskRoute = taskRouter;
 
 const app = express();
 const RedisStore = connectRedis(session);
-
-app.use(session({
-  store:new RedisStore({client:redisClient}), //stores session in Redis
-  secret:process.env.SESSION_SECRET!,
-  resave:false,
-  saveUninitialized:false,
-  cookie:{
-    httpOnly:true,
-    secure: process.env.NODE_ENV === "production",
-    maxAge:1000*60*60*24 //one day
-  }
-}))
-
-// to parse the json body:
-app.use(express.json());
 
 const corsOptions = {
   origin: "http://localhost:3000", // 👈 TEMPORARY for testing, not secure for production
@@ -45,6 +28,23 @@ const corsOptions = {
 app.use(cors(corsOptions));
 
 app.use(cookieParser());
+
+app.use(
+  session({
+    store: new RedisStore({ client: redisClient }), //stores session in Redis
+    secret: process.env.SESSION_SECRET!,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      maxAge: 1000 * 60 * 60 * 24, //one day
+    },
+  })
+);
+
+// to parse the json body:
+app.use(express.json());
 
 //routes:
 app.get("/", (_req: Request, res: Response) => {
@@ -61,6 +61,29 @@ app.use(errorHandlerMiddleware);
 
 console.log(`Worker ${process.pid} connected to Redis`);
 
+const port = Number(process.env.PORT) || 5000;
 
-export default app; //👈👈👈👈🛑🛑🛑 Export the app here 🛑🛑🛑👈👈👈👈
+const startWorker = async () => {
+  try {
+    //👇 MongoDB and Node.js are running outside Docker
+    // 👇(both on the laptop)
 
+    // await mongoose.connect("mongodb://localhost:27017/TASK-MANAGER");
+
+    //👇database is running locally on my machine and Node app is running inside Docker
+    // await mongoose.connect("mongodb://host.docker.internal:27017/TASK-MANAGER");
+
+    //👇 connecting to mongodb running inside adocker container
+    await mongoose.connect("mongodb://mongodb:27017/TASK-MANAGER");
+    console.log(`Worker ${process.pid} connected to DB`);
+
+    app.listen(port, () => {
+      console.log(`Worker ${process.pid} listening on port ${port}`);
+    });
+  } catch (error) {
+    console.log(`Worker ${process.pid} failed to connect:`, error);
+  }
+};
+startWorker();
+
+// export default app; //👈👈👈👈🛑🛑🛑 Export the app here 🛑🛑🛑👈👈👈👈
